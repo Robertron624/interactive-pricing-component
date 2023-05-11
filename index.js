3914af90590cb95347d9cd26b26b068e3e40f86e
Roberto Ramirez <robert.624@hotmail.com>
2023-05-11 16:12:15 -0500

    js separated and mobile design done

---
+++ index.js
@@ -0,0 +1,93 @@
+const prices = [8, 12, 16, 24, 36];
+const pricesAnnual = [72, 108, 144, 216, 324]; // with a 25% discount
+const pageViewValuesMonthly = [10, 50, 100, 500, "1M"];
+const pageViewsValuesAnnual = [600, 900, "1.2M", "1.8M", "2.4M"];
+const timeElement = document.querySelector(".time");
+
+let isYearly = false;
+
+const sliderInput = document.getElementById("myRange");
+const price = document.querySelector(".price");
+const pageviews = document.querySelector(".pageviews");
+const discountElement = document.querySelector(".discount");
+
+const toggleBackground = document.querySelector(".toggler");
+
+const toggle_checkbox = document.querySelector(
+    ".switch input[type='checkbox']"
+);
+
+toggle_checkbox.addEventListener("change", function () {
+    if (this.checked) {
+        isYearly = true;
+
+        toggleBackground.classList.add("toggler--on");
+
+        let pageViewToDisplay = pageViewsValuesAnnual[sliderInput.value];
+
+        // Add K to the pageviews if the value is a number (not a string)
+        if (typeof pageViewToDisplay === "number") {
+            pageViewToDisplay += "K PAGEVIEWS";
+        } else {
+            pageViewToDisplay = pageViewToDisplay + " PAGEVIEWS";
+        }
+
+        timeElement.innerHTML = " / year";
+        price.innerHTML =
+            "$" +
+            pricesAnnual[sliderInput.value] + ".00";
+        pageviews.innerHTML = pageViewToDisplay;
+    } else {
+        isYearly = false;
+
+        toggleBackground.classList.remove("toggler--on");
+
+        let pageViewToDisplay = pageViewValuesMonthly[sliderInput.value];
+
+        // Add K to the pageviews if the value is a number (not a string)
+        if (typeof pageViewToDisplay === "number") {
+            pageViewToDisplay += "K PAGEVIEWS";
+        } else {
+            pageViewToDisplay = pageViewToDisplay + " PAGEVIEWS";
+        }
+
+        timeElement.innerHTML = " / month";
+        price.innerHTML = "$" + prices[sliderInput.value] + ".00";
+        pageviews.innerHTML = pageViewToDisplay;
+    }
+});
+
+sliderInput.oninput = function () {
+    let value = sliderInput.value;
+
+    let priceToDisplay = isYearly
+        ? pricesAnnual[value]
+        : prices[value];
+
+    price.innerHTML = "$" + priceToDisplay + ".00";
+
+    let pageViewToDisplay = isYearly
+        ? pageViewsValuesAnnual[value]
+        : pageViewValuesMonthly[value];
+
+
+    if (typeof pageViewToDisplay === "number") {
+        pageViewToDisplay += "K PAGEVIEWS";
+    } else {
+        pageViewToDisplay = pageViewToDisplay + " PAGEVIEWS";
+    }
+
+    pageviews.innerHTML = pageViewToDisplay;
+};
+
+// Update the slider value when the user drags the slider to apply the CSS color
+sliderInput?.addEventListener("input", (e) => {
+    if (e.target) {
+        const sliderPercentage = e.target.value * 25;
+
+        sliderInput.style.setProperty(
+            "--p",
+            sliderPercentage + "%"
+        );
+    }
+});
